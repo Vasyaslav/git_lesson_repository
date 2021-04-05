4035788f1ce432cc3ee207838710930181aaf436
Vasyaslav <rkbrktyl@gmail.com>
2021-04-05 18:51:19 +0300

    Добавлены минимальные формы(для авторизации и регистрации пользователя), базовый шаблон с моими контактами в конце страницы, файлы связанные с работо БД и модель пользователя в ДБ. Также добавлен обработцик адресов для регистрации и авторизации, но пока нет шаблонов в формате html для них потэтому они могут только возвращать json.

---
+++ data/users.py
@@ -0,0 +1,28 @@
+import datetime
+import sqlalchemy
+from .db_session import SqlAlchemyBase
+from sqlalchemy_serializer import SerializerMixin
+from werkzeug.security import check_password_hash, generate_password_hash
+from flask_login import UserMixin
+
+
+class User(SqlAlchemyBase, UserMixin, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    surname = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    email = sqlalchemy.Column(sqlalchemy.String, unique=True, nullable=True)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    modified_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
+
+    def __repr__(self):
+        return f'<colonist> {self.id} {self.surname} {self.name}'
+
+    def hashing_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
+
---
+++ main.py
@@ -0,0 +1,69 @@
+from flask import Flask, jsonify, render_template, redirect, request
+from flask_login import LoginManager, logout_user, login_user, login_required, current_user
+from data import db_session
+from data.users import User
+from forms.user import RegisterForm, LoginForm
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'some really secret key'
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+
+@app.route('/')
+def start_page():
+    db_sess = db_session.create_session()
+
+    return render_template('base.html', title='Начальная страница')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def registration():
+    form = RegisterForm
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        if form.password.data != form.password.repeat.data:
+            return jsonify({'Ошибка': 'Пароли не совпадают.'})
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return jsonify({'Ошибка': 'Пользователь с такой почтой уже существует.'})
+        user = User(surname=form.surname.data,
+                    name=form.name.data,
+                    email=form.email.data)
+        user.hashing_password(form.password.data)
+        db_sess.commit()
+        return jsonify({'Успех': 'Но шаблоны ещё не созданы...'})
+    return jsonify({'Ошибка': 'Не все поля заполнены.'})
+
+
+@app.route('/login', methods=['GET'])
+def login():
+    form = LoginForm
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return jsonify({'Успех': 'вы залогинились.'})
+        return jsonify({'Ошибка': 'Неправильный email или пароль.'})
+
+
+def main():
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
