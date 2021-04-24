c140be83129d95d5ec75630fe8f8fe0500c85a2a
Vasyaslav <rkbrktyl@gmail.com>
2021-04-24 17:31:23 +0300

    Ещё попытка

--- vk.py
+++ vk.py
@@ -4,51 +4,69 @@ from data import db_session
 from data.vks import VkInfo
 import random
 import schedule
-from multiprocessing import Process, freeze_support
+from multiprocessing import Process
 
 
 class VkTools():
     def __init__(self):
-        processes = {}
+        self.processes = {}
         self.main()
-
-    def sub_count(self, bot, dbsess, vksess, adm_id):
-        vk = vksess.get_api()
-        vk.messages.send(user_id=adm_id,
-                         message=f'''Кол-во новых подписок на сообщество: {bot.plus_sub_count}\n
-                                 Кол-во отписок: {bot.minus_sub_count}''',
-                         random_id=random.randint(0, 2 ** 64))
-        bot.plus_sub_count = 0
-        bot.minus_sub_count = 0
-        dbsess.commit()
-
-    def for_bots(self, bot, sess):
-        vk_session = vk_api.VkApi(token=bot.gr_token)
-        longpoll = VkBotLongPoll(vk_session, bot.gr_id)
-        admin_id = bot.admin_id
-        schedule.every().day.at('21:52').do(self.sub_count, vksess=vk_session, bot=bot, adm_id=admin_id, dbsess=sess)
-        for event in longpoll.listen():
-            if event.type == VkBotEventType.GROUP_JOIN:
-                bot.plus_sub_count += 1
-                sess.commit()
-            if event.type == VkBotEventType.GROUP_LEAVE:
-                bot.minus_sub_count += 1
-                sess.commit()
-            if event.type == VkBotEventType.MESSAGE_NEW:
-                vk = vk_session.get_api()
-                vk.messages.send(user_id=event.obj.message['from_id'],
-                                 message=f"{bot.greetings}",
-                                 random_id=random.randint(0, 2 ** 64))
 
     def main(self):
         db_session.global_init('db/storage.db')
         db_sess = db_session.create_session()
         all_bots = db_sess.query(VkInfo).all()
         for bot in all_bots:
-            b = Process(target=self.for_bots, args=(bot, db_sess))
-            b.start()
+            self.processes[str(bot.id)] = Process(target=self.for_bots, args=(bot,))
+            self.processes[str(bot.id)].start()
+            schedule.every().day.at('17:26').do(self.sub_count, bot=bot)
+        while True:
+            schedule.run_pending()
+
+    def terminator(self, some_id):
+        for i in self.processes:
+            if str(some_id) == i:
+                self.processes[i].terminate()
+
+    def reload(self):
+        for i in self.processes:
+            self.processes[i].terminate()
+        self.main()
+
+    def sub_count(self, bot):
+        db_session.global_init('db/storage.db')
+        db_sess = db_session.create_session()
+        vk_session = vk_api.VkApi(token=bot.gr_token)
+        vk = vk_session.get_api()
+        vk.messages.send(user_id=bot.admin_id,
+                         message=f'''Кол-во новых подписок на сообщество: {bot.plus_sub_count}\n
+                                 Кол-во отписок: {bot.minus_sub_count}''',
+                         random_id=random.randint(0, 2 ** 64))
+        bot.plus_sub_count = 0
+        bot.minus_sub_count = 0
+        db_sess.commit()
+
+    def for_bots(self, bot):
+        db_session.global_init('db/storage.db')
+        db_sess = db_session.create_session()
+        vk_session = vk_api.VkApi(token=bot.gr_token)
+        longpoll = VkBotLongPoll(vk_session, bot.gr_id)
+        admin_id = bot.admin_id
+        schedule.every(5).seconds.do(self.sub_count, vksess=vk_session, bot=bot, adm_id=admin_id)
+        for event in longpoll.listen():
+            if event.type == VkBotEventType.GROUP_JOIN:
+                bot.plus_sub_count += 1
+                db_sess.commit()
+            if event.type == VkBotEventType.GROUP_LEAVE:
+                bot.minus_sub_count += 1
+                db_sess.commit()
+            if event.type == VkBotEventType.MESSAGE_NEW:
+                vk = vk_session.get_api()
+                vk.messages.send(user_id=event.obj.message['from_id'],
+                                 message=f"{bot.greetings}",
+                                 random_id=random.randint(0, 2 ** 64))
 
 
 if __name__ == "__main__":
-    freeze_support()
     VkTools()
+    print(14)
